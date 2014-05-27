dbd59a592acdb63de1fc0550364af552b13f39f7
Ankush <ankush@fab.com>
2014-05-27 13:32:49 +0530

    Solution.

---
+++ ReverseShuffleMerge.java
@@ -0,0 +1,13 @@
+package hackkerrank;
+
+public class ReverseShuffleMerge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
